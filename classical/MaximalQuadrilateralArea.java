a3d548a28ec94eeb8f5a2ef487eb2c1f56e0fd3a
Balaji Ramasubramanian <Balaji@Balajis-MacBook-Air.local>
2017-11-16 20:15:39 +0530

    Maximal Quadrilateral Area

--- classical/MaximalQuadrilateralArea.java
+++ classical/MaximalQuadrilateralArea.java
@@ -4,7 +4,6 @@ class MaximalQuadrilateralArea
 {
     public static void main(String[] args)
    {
-//       int t,n,m;
       Scanner s=new Scanner(System.in);
        int t;
        t=s.nextInt();
@@ -12,15 +11,15 @@ class MaximalQuadrilateralArea
        {
             double a,b,c,d;
             double area,s1;
-       a=s.nextDouble();
-       b=s.nextDouble();      
-       c=s.nextDouble();
-       d=s.nextDouble();
-      s1=(a+b+c+d)/2; 
+            a=s.nextDouble();
+            b=s.nextDouble();      
+            c=s.nextDouble();
+            d=s.nextDouble();
+            s1=(a+b+c+d)/2; 
       area=  sqrt((s1-a)*(s1-b)*(s1-c)*(s1-d)) ; 
-       System.out.println(area);
-
-    t--;}
+         System.out.println(area);
+         t--;
+      }
    }
 
 
