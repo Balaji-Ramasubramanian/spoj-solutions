4978e6951f225b8ee823f2ec53d58ff460264cfe
Balaji Ramasubramanian <Balaji@Balajis-MacBook-Air.local>
2017-11-16 23:11:30 +0530

    AMR12D - The Mirror of Galadriel

---
+++ classical/TheMirrorOfGaladriel.java
@@ -0,0 +1,43 @@
+// Problem URL : http://www.spoj.com/submit/AMR12D/id=17970167
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+ 
+class TheMirrorOfGaladriel
+{
+   public static void main(String args[])
+   {
+      String str;
+      int i,t,c, length,flag=0;
+      Scanner s= new Scanner(System.in);
+      t=s.nextInt();
+      while(t>0)
+      {boolean b=true;
+      str = s.next();
+      length = str.length();   
+
+    for ( i = 0; i < str.length(); i++) 
+    {
+        for (int j = i+1; j <= str.length(); j++)
+        {
+           String rev = new StringBuffer(str.substring(i,j)).reverse().toString();
+            if(!str.contains(rev))
+            {
+                 b=false;
+                 break;
+            }
+                
+        }
+            break;   
+    }  
+
+    if(b==false)
+       System.out.println("NO");
+    else
+       System.out.println("YES");
+ 
+      t--;
+      } 
+ 
+   }
+}
